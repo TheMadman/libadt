29bb89f5be2bcf5aab4af1ac1fbc37e1a6260526
Marcus Harrison <marcus@harrisonland.co.uk>
2024-12-03 23:49:05 +0100

    Delete errant backslashes

--- src/libadt/lptr.h
+++ src/libadt/lptr.h
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-\
+
 #ifndef LIBADT_LPTR
 #define LIBADT_LPTR
 
