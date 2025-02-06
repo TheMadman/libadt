82e178c1d22868ec1567487d6560c71b556eb0b9
Marcus Harrison <marcus@harrisonland.co.uk>
2025-02-06 14:26:31 +0100

    Add header like I was supposed to the first time

---
+++ src/libadt/init.h
@@ -0,0 +1,37 @@
+/*
+ * libadt - A library containing abstract data types
+ * Copyright (C) 2024   Marcus Harrison
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBADT_INIT
+#define LIBADT_INIT
+
+// Mark functions as external
+// We don't have compiler-specific flags
+// here: just assume the human compiling
+// the project knows the compiler and has
+// configured the environment.
+//
+// If the environment isn't set up, just use
+// the compiler defaults.
+//
+// For example: for GCC, set
+// CFLAGS+="-fvisibility=hidden -fvisibility-inline-hidden -DEXPORT="'__attribute__((visibility("hidden")))"'
+#ifndef EXPORT
+#define EXPORT
+#endif
+
+#endif
