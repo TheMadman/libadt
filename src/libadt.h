0219072c5329b1c844af047b7e6506205b11c4ee
Marcus Harrison <marcus@harrisonland.co.uk>
2025-05-13 10:18:03 +0200

    Add include-all header

---
+++ src/libadt.h
@@ -0,0 +1,37 @@
+/*
+ * libadt - A library containing abstract data types
+ * Copyright (C) 2024   Marcus Harrison
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBADT_H
+#define LIBADT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "libadt/init.h"
+#include "libadt/util.h"
+#include "libadt/lptr.h"
+#include "libadt/str.h"
+#include "libadt/bitwise_array.h"
+#include "libadt/vector.h"
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // LIBADT_H
