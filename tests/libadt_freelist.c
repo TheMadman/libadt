d6f57f4b40eb30bd9383ff2c85ec8275bbe7e7a1
Marcus Harrison <marcus@harrisonland.co.uk>
2026-09-24 11:47:31 +0200

    Fix copyright notice on test

--- tests/libadt_freelist.c
+++ tests/libadt_freelist.c
@@ -1,6 +1,6 @@
 /*
- * Project Name - Project Description
- * Copyright (C) 2026
+ * libadt - A library containing abstract data types
+ * Copyright (C) 2026  Marcus Harrison
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
